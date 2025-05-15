10f4af8ca7296ff28974076b3389a0c619488128
ninimefarishvili <164371339+ninimefarishvili@users.noreply.github.com>
2025-05-15 20:25:18 +0400

    Update models.py

--- models.py
+++ models.py
@@ -8,3 +8,31 @@ class Quest:
   @staticmethod
   def from_line(line):
     return Quest(line.strip())
+
+
+class TaskQueue:
+  def __init__(self, filename = queue.txt):
+    self.filename = filename
+
+  def add_task(self, quest):
+    with open (self.filename, "a") as f:
+      f.write(quest.to_line())
+
+  def get_task(self):
+    with open (self.filename, "r") as f:
+      lines = f.readlines()
+
+import random
+class Worker:
+  def __init__(self, name):
+    self.name = name
+
+  def process_quest(self, quest):
+    print(quest.description)
+    success = random.random() < 0.8
+
+    if success:
+      print(f"{self.name} warmatebit shesrulda questi")
+
+    else:
+      print(f"{self.name} ver shesrulda questi")
